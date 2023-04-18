6a8f9d6f17cbc3660c7173eeae2944c71c869f29
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-04-18 15:58:16 +0300

    initial commit

---
+++ js/pages/home.js
@@ -0,0 +1,37 @@
+// IMPORTS
+
+
+// FUNCTION EXECUTION
+
+/* header: start */
+/* header: end */
+
+/* hero: start */
+/* hero: end */
+
+/* about: start */
+/* about: end */
+
+/* services: start */
+/* services: end */
+
+/* resume: start */
+/* resume: end */
+
+/* freelance: start */
+/* freelance: end */
+
+/* portfolio: start */
+/* portfolio: end */
+
+/* testimonials: start */
+/* testimonials: end */
+
+/* blog: start */
+/* blog: end */
+
+/* contact: start */
+/* contact: end */
+
+/* footer: start */
+/* footer: end */
